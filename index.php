d95f56fcf5f7481181d9b19fd1abc42fdbc01328
Jomeish <113467417+Jomeish@users.noreply.github.com>
2023-10-20 19:38:10 +0000

    update1

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-  <!--
-  pluh
-  -->
+   sdfghjk
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
   </body>
 </html>
